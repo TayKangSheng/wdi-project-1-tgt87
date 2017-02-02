0817ac41e11bbccec84cebdb198527d6b8fa0bf9
tgt87 <golduck21187@yahoo.com.sg>
2017-02-02 21:18:20 +0800

    Instructions edited

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Treasure Hunt
-A strategy game where the player builds his own path and collects items (sword, shield & key) along the way in order to defeat a monster and obtain the treasure.
+A strategy game where the player builds his/her own path and collects items (sword, shield & key) along the way in order to defeat a monster and obtain the treasure.
 
 ## Getting Started
 It is best to use Chrome and you can play the game [here](https://wdi-sg.github.io/wdi-project-1-tgt87/).
 
 ### How to play
-* Clicking on the 'Place tiles' button will enable you to place 2 tiles on the board.
+* Clicking on the 'Place tiles' button enables you to place 2 tiles on the board.
 * You'll need to click the button each time you wish to place tiles on the board.
 * Each click on the button (except the first click) will cost you 1 turn.
 * To place the tiles on the board, click on the grid that you wish to place your tiles on.
--- assets/js/project1.js
+++ assets/js/project1.js
@@ -1,14 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
-// Draw 50 div to be use as grids for game board
   var board = document.getElementById('board')
   for (var i = 49; i >= 0; i--) {
     var div = document.createElement('div')
     div.setAttribute('data-num', i)
-    div.textContent = i
+    // div.textContent = i
     board.appendChild(div)
   }
 
-  // Fixed player, treasure chest and monster starting position on the board
   var boy = document.getElementById('boy')
   var box2 = document.querySelector("[data-num='2']")
   box2.setAttribute('class', 'colored')
@@ -23,28 +21,24 @@ document.addEventListener('DOMContentLoaded', function () {
   var box42 = document.querySelector("[data-num='42']")
   box42.appendChild(monster)
 
-// Randomise sword, shield and key positions and make sure they don't appear on the same grid by having an array to track the unoccupied grids
   var availBoxes = [0, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 43, 44, 45, 46, 48, 49]
 
   var sword = document.getElementById('sword')
   var randSword = Math.floor(Math.random() * 47)
   document.querySelector('[data-num=' + '"' + availBoxes[randSword] + '"' + ']').appendChild(sword)
   var swordPos = parseInt(document.getElementById('sword').parentElement.dataset.num)
-  console.log('swordPos', swordPos)
   availBoxes.splice(randSword, 1)
 
   var shield = document.getElementById('shield')
   var randShield = Math.floor(Math.random() * 46)
   document.querySelector('[data-num=' + '"' + availBoxes[randShield] + '"' + ']').appendChild(shield)
   var shieldPos = parseInt(document.getElementById('shield').parentElement.dataset.num)
-  console.log('shieldPos', shieldPos)
   availBoxes.splice(randShield, 1)
 
   var key = document.getElementById('key')
   var randKey = Math.floor(Math.random() * 45)
   document.querySelector('[data-num=' + '"' + availBoxes[randKey] + '"' + ']').appendChild(key)
   var keyPos = parseInt(document.getElementById('key').parentElement.dataset.num)
-  console.log('keyPos', keyPos)
   availBoxes.splice(randKey, 1)
 
   var boxes = document.querySelector('#board').querySelectorAll('div')
@@ -54,35 +48,26 @@ document.addEventListener('DOMContentLoaded', function () {
   var tilesCount = 0
   var gameOver = false
 
-  for (var i = 0; i < boxes.length; i++) {
-    if (i !== 47) {
-      boxes[i].addEventListener('click', function (e) {
-        var boxIndex = parseInt(e.target.dataset.num)
-        console.log(boxIndex)
-        if (isNaN(boxIndex)) {
-          boxIndex = parseInt(e.target.parentElement.dataset.num)
-          if (!tilesPos.includes(boxIndex) && tilesCount > 0) {
-            tilesPos.push(boxIndex)
-            e.target.parentElement.setAttribute('class', 'colored')
-            tilesCount--
-          }
-          console.log('if NaN',boxIndex)
-        } else {
-          if (!tilesPos.includes(boxIndex) && tilesCount > 0) {
-            tilesPos.push(boxIndex)
-            e.target.setAttribute('class', 'colored')
-            tilesCount--
+  for (var j = 0; j < boxes.length; j++) {
+    boxes[j].addEventListener('click', function (e) {
+      var boxIndex = parseInt(e.target.dataset.num)
+      if (isNaN(boxIndex)) {
+        boxIndex = parseInt(e.target.parentElement.dataset.num)
+        if (!tilesPos.includes(boxIndex) && tilesCount > 0) {
+          tilesPos.push(boxIndex)
+          e.target.parentElement.setAttribute('class', 'colored')
+          tilesCount--
         }
-           }
-          // tilesCount--
-          console.log('tilesPos after push', tilesPos)
-          console.log(tilesPos)
-      })
-    }
+      } else {
+        if (!tilesPos.includes(boxIndex) && tilesCount > 0) {
+          tilesPos.push(boxIndex)
+          e.target.setAttribute('class', 'colored')
+          tilesCount--
+        }
+      }
+    })
   }
 
-// Each time the Put Tiles button is clicked, putTiles function will be called to check if it's valid position to place the tile.
-// No. of turns will reduce by 1 and when it reaches 0 the button will be disabled to disallow putting of tiles.
   tilesBtn.addEventListener('click', function () {
     turnCount--
     tilesCount = 2
@@ -92,7 +77,6 @@ document.addEventListener('DOMContentLoaded', function () {
       tilesBtn.disabled = true
       h2.textContent = 'You have run out of turns. Game Over!'
     }
-    console.log('turnCount', turnCount)
   })
 
   var swordCount = 0
@@ -100,7 +84,6 @@ document.addEventListener('DOMContentLoaded', function () {
   var keyCount = 0
 
   document.addEventListener('keydown', function (e) {
-    console.log('tilesPos inside movePlayer', tilesPos)
     if ([37, 38, 39, 40].indexOf(e.keyCode) > -1) {
       e.preventDefault()
     }
@@ -109,7 +92,6 @@ document.addEventListener('DOMContentLoaded', function () {
       case 37:  // left
         boyCurrentPos += 1
         if (tilesPos.indexOf(boyCurrentPos) !== -1) {
-          console.log(boyCurrentPos)
           document.querySelector('[data-num=' + '"' + boyCurrentPos + '"' + ']').appendChild(boy)
           getItems()
         } else {
@@ -120,7 +102,6 @@ document.addEventListener('DOMContentLoaded', function () {
       case 38:  // up
         boyCurrentPos += 5
         if (tilesPos.indexOf(boyCurrentPos) !== -1) {
-          console.log(boyCurrentPos)
           document.querySelector('[data-num=' + '"' + boyCurrentPos + '"' + ']').appendChild(boy)
           getItems()
         } else {
@@ -131,7 +112,6 @@ document.addEventListener('DOMContentLoaded', function () {
       case 39:  // right
         boyCurrentPos -= 1
         if (tilesPos.indexOf(boyCurrentPos) !== -1) {
-          console.log(boyCurrentPos)
           document.querySelector('[data-num=' + '"' + boyCurrentPos + '"' + ']').appendChild(boy)
           getItems()
         } else {
@@ -142,7 +122,6 @@ document.addEventListener('DOMContentLoaded', function () {
       case 40:  // down
         boyCurrentPos -= 5
         if (tilesPos.indexOf(boyCurrentPos) !== -1) {
-          console.log(boyCurrentPos)
           document.querySelector('[data-num=' + '"' + boyCurrentPos + '"' + ']').appendChild(boy)
           getItems()
         } else {
@@ -173,21 +152,20 @@ document.addEventListener('DOMContentLoaded', function () {
 
   var h2 = document.querySelector('h2')
   function isGameOver () {
-    console.log('turn inside GO', turnCount)
     if (document.querySelector('[data-num=' + '"' + boyCurrentPos + '"' + ']').childElementCount === 2 && boyCurrentPos === 42 && turnCount > 0) {
       if (swordCount === 1 && shieldCount === 1) {
-        document.querySelector('[data-num=' + '"' + boyCurrentPos + '"' + ']').removeChild(document.querySelector('[data-num=' + '"' + boyCurrentPos + '"' + ']').firstElementChild)
+        monster.parentNode.removeChild(monster)
         monsterDefeat = true
         h2.textContent = 'Monster defeated!'
       } else {
         gameOver = true
         tilesBtn.disabled = true
         h2.textContent = 'You are killed by the monster! Game Over!'
-        document.querySelector('[data-num=' + '"' + boyCurrentPos + '"' + ']').removeChild(document.querySelector('[data-num=' + '"' + boyCurrentPos + '"' + ']').lastElementChild)
+        boy.parentNode.removeChild(boy)
       }
     } else if (document.querySelector('[data-num=' + '"' + boyCurrentPos + '"' + ']').childElementCount === 2 && boyCurrentPos === 47 && turnCount > 0) {
       if (keyCount === 1 && monsterDefeat === true) {
-        document.querySelector('[data-num=' + '"' + boyCurrentPos + '"' + ']').removeChild(document.querySelector('[data-num=' + '"' + boyCurrentPos + '"' + ']').firstElementChild)
+        chest.parentNode.removeChild(chest)
         h2.textContent = 'Treasure Obtained! You won!'
         gameOver = true
         tilesBtn.disabled = true
